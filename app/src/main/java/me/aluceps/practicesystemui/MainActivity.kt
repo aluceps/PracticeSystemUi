355d8c58a66777ee1381a726fa382beb976398ff
aluceps <aluceps@gmail.com>
2018-10-16 01:44:05 +0900

    reformat

--- app/src/main/java/me/aluceps/practicesystemui/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicesystemui/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.aluceps.practicesystemui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
