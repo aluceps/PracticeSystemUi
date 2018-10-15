07b62864a5970ff773d45ba502ef463e95447b5a
aluceps <aluceps@gmail.com>
2018-10-16 02:23:12 +0900

    add function that show/hide statusbar and navigation

--- app/src/main/java/me/aluceps/practicesystemui/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicesystemui/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.toolbar)
         supportFragmentManager.beginTransaction()
                 .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
--- app/src/main/java/me/aluceps/practicesystemui/MainFragment.kt
+++ app/src/main/java/me/aluceps/practicesystemui/MainFragment.kt
@@ -18,6 +18,26 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            toggleSystemUi()
+        }
+    }
+
+    private fun toggleSystemUi() {
+        activity?.window?.decorView?.run {
+            systemUiVisibility = if (systemUiVisibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+                (activity as? MainActivity)?.supportActionBar?.hide()
+                View.SYSTEM_UI_FLAG_IMMERSIVE +
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE +
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN +
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION +
+                        View.SYSTEM_UI_FLAG_FULLSCREEN +
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            } else {
+                (activity as? MainActivity)?.supportActionBar?.show()
+                View.SYSTEM_UI_FLAG_VISIBLE
+            }
+        }
     }
 
     companion object {
